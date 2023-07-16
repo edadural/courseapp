f88158dc3fe2776a92069bc5723d6529050e97b6
Eda Dural <edaadurall@gmail.com>
2023-07-16 17:12:23 +0300

    Admin

--- courses/admin.py
+++ courses/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Course, Category
 
 # Register your models here.
+
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ("title","isActive","slug",)
+    list_display_links = ("title","slug",)
+    readonly_fields = ("slug",)
+    list_filter = ("title","isActive")
+    list_editable = ("isActive",)
+    search_fields = ("title","description",)
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    pass
--- courses/models.py
+++ courses/models.py
@@ -7,15 +7,18 @@ class Course(models.Model):
     imageUrl = models.CharField(max_length=50, blank=False)
     date = models.DateField()
     isActive = models.BooleanField()
-    slug = models.SlugField(default="", null=False, unique=True, db_index=True)  # varolan kayıtlar için "", zorunlu alan false, unique benzersiz, db primary key icin aktif edilmesi
+    slug = models.SlugField(default="", blank=True, editable=False, null=False, unique=True, db_index=True)  # varolan kayıtlar için "", blank ilgili formda bos deger girmek, formdaki slug bilgisi editlenmesin, zorunlu alan false, unique benzersiz, db primary key icin aktif edilmesi
 
     def save(self, *args, **kwargs):
         self.slug = slugify(self.title)  # url parametresini duzgun bi sekilde olusturur
         super().save(args, kwargs)
 
     def __str__(self):
-        return f"{self.title} {self.date}"
+        return f"{self.title}"
     
 class Category(models.Model):
     name = models.CharField(max_length=40)
     slug = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f"{self.name}"
