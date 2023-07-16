f4aa5c6d126773e4efc12ba6144a598636300de7
Eda Dural <edaadurall@gmail.com>
2023-07-16 07:21:07 +0300

    Templates

--- courses/urls.py
+++ courses/urls.py
@@ -3,7 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
-    path('list', views.kurslar),
     path('<kurs_adi>', views.details),
     path('kategori/<int:category_id>', views.getCoursesByCategoryId),
     path('kategori/<str:category_name>', views.getCoursesByCategory, name='courses_by_category'),
--- courses/views.py
+++ courses/views.py
@@ -9,19 +9,11 @@ data = {
 }
 
 def index(request):
-    return render(request, 'courses/index.html')
-
-def kurslar(request):
-    list_items = ""
     category_list = list(data.keys())
 
-    for category in category_list:
-        redirect_url = reverse('courses_by_category', args=[category])
-        list_items += f"<li><a href='{redirect_url}'>{category}</a></li>"
-
-    html = f"<h1>kurs listesi</h1><ul>{list_items}</ul>"
-
-    return HttpResponse(html)
+    return render(request, 'courses/index.html', {
+        'categories': category_list
+    })
 
 def details(request, kurs_adi):
     return HttpResponse(f"{kurs_adi} detay sayfası")
@@ -29,12 +21,14 @@ def details(request, kurs_adi):
 def getCoursesByCategory(request, category_name):
     try:
         category_text = data[category_name]    
-        return HttpResponse(category_text)
+        return render(request, 'courses/kurslar.html', {
+            'category': category_name,
+            'category_text': category_text
+        })
     except:
         return HttpResponseNotFound("<h1>yanlış kategori seçimi<h1>")
 
 def getCoursesByCategoryId(request, category_id):
-    # 1-2-3 => 
     category_list = list(data.keys())
 
     if(category_id > len(category_list)):
