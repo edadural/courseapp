c456952ea3c567b08739b98c030a4764dd437231
Eda Dural <edaadurall@gmail.com>
2023-07-16 10:16:25 +0300

    Model - Migration

--- courses/models.py
+++ courses/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Course(models.Model):
+    title = models.CharField(max_length=50)
+    description = models.TextField()
+    imageUrl = models.CharField(max_length=50, blank=False)
+    date = models.DateField()
+    isActive = models.BooleanField()
