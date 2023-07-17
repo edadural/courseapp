01d15dfda09a18603ba22c7108cdbb5eed74827c
Eda Dural <edaadurall@gmail.com>
2023-07-17 21:33:06 +0300

    form bilgilerinin alınması

--- courses/views.py
+++ courses/views.py
@@ -13,6 +13,38 @@ def index(request):
     })
 
 def create_course(request):
+    if request.method == "POST":
+        title = request.POST["title"]
+        description = request.POST["description"]
+        imageUrl = request.POST["imageUrl"]
+        slug = request.POST["slug"]
+        isActive = request.POST.get("isActive", False)
+        isHome = request.POST.get("isHome", False)
+        
+        if isActive == "on":
+            isActive = True
+
+        if isHome == "on":
+            isHome = True
+
+        error = False
+        msg = ""
+
+        if title == "":
+            error = True
+            msg += "Title zorunlu bir alan. "
+
+        if len(title) < 5:
+            error = True
+            msg += "Title için en az 5 karakter girmelisiniz. "
+
+        if error:
+            return render(request, "courses/create-course.html", { "error": True, "msg": msg })
+
+        kurs = Course(title=title, description=description, imageUrl=imageUrl, slug=slug, isActive=isActive, isHome=isHome)
+        kurs.save()
+        return redirect("/kurslar")
+        
     return render(request, "courses/create-course.html")
 
 def search(request):
