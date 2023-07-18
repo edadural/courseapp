8c08835f9278c73b4cbf810eae5768babc7c0445
Eda Dural <edaadurall@gmail.com>
2023-07-18 19:41:00 +0300

    login - logout

--- account/views.py
+++ account/views.py
@@ -1,10 +1,28 @@
 from django.shortcuts import redirect, render
+from django.contrib.auth import authenticate, login, logout
 
 def user_login(request):
-    return render(request, "account/login.html")
+    if request.user.is_authenticated:  # kullanıcının login olup olmadığı
+        return redirect("index")
+
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect("index")
+        else:
+            return render(request, "account/login.html", {"error":"username ya da password yanlış"})
+
+    else:
+        return render(request, "account/login.html")
 
 def user_register(request):
     return render(request, "account/register.html")
 
 def user_logout(request):
+    logout(request)
     return redirect("index")
