d58ea3097685ca10c1e28e5cf63ac29fdd36d793
Eda Dural <edaadurall@gmail.com>
2023-07-18 20:35:34 +0300

    register

--- account/views.py
+++ account/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import redirect, render
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
 
 def user_login(request):
     if request.user.is_authenticated:  # kullanıcının login olup olmadığı
@@ -21,7 +22,42 @@ def user_login(request):
         return render(request, "account/login.html")
 
 def user_register(request):
-    return render(request, "account/register.html")
+    if request.method == "POST":
+        username = request.POST["username"]
+        email = request.POST["email"]
+        password = request.POST["password"]
+        repassword = request.POST["repassword"]
+
+        if password != repassword:
+            return render(request, "account/register.html", 
+            {
+                "error":"parola eşleşmiyor.", 
+                "username": username,
+                "email": email
+            })
+        
+        if User.objects.filter(username = username).exists():
+                return render(request, "account/register.html", 
+            {
+                "error":"username kullanılıyor.", 
+                "username": username,
+                "email": email
+            })
+
+        if User.objects.filter(email = email).exists():
+            return render(request, "account/register.html", 
+            {
+                "error":"email kullanılıyor.",
+                "username": username,
+                "email": email
+            })
+
+        user = User.objects.create_user(username=username, email=email, password=password)                    
+        user.save()
+        return redirect("user_login")
+            
+    else:
+        return render(request, "account/register.html")
 
 def user_logout(request):
     logout(request)
