b09aed44afc7a708ec1d9ca0317c1d036ab5c40c
Eda Dural <edaadurall@gmail.com>
2023-07-19 08:41:24 +0300

    Messages

--- account/views.py
+++ account/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import redirect, render
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.models import User
+from django.contrib import messages
 
 def user_login(request):
     if request.user.is_authenticated and "next" in request.GET:  # kullanıcının login olup olmadığı, icinde next varsa yonlendirmek yerine mesaj
@@ -14,13 +15,15 @@ def user_login(request):
 
         if user is not None:
             login(request, user)
+            messages.add_message(request, messages.SUCCESS, "Giriş başarılı")
             nextUrl = request.GET.get("next", None)
             if nextUrl is None:
                 return redirect("index")
             else:
                 return redirect(nextUrl)
         else:
-            return render(request, "account/login.html", {"error":"username ya da password yanlış"})
+            messages.add_message(request, messages.ERROR, "username ya da password yanlış")
+            return render(request, "account/login.html")
 
     else:
         return render(request, "account/login.html")
@@ -64,5 +67,6 @@ def user_register(request):
         return render(request, "account/register.html")
 
 def user_logout(request):
+    messages.add_message(request, messages.SUCCESS, "Çıkış başarılı")
     logout(request)
     return redirect("index")
--- courseapp/settings.py
+++ courseapp/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+from django.contrib.messages import constants as messages
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -134,3 +135,11 @@ MEDIA_URL = "/media/"
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_URL = "/account/login"
+
+MESSAGE_TAGS = {
+    messages.DEBUG: "alert-secondary",
+    messages.INFO: "alert-info",
+    messages.SUCCESS: "alert-success",
+    messages.WARNING: "alert-warning",
+    messages.ERROR: "alert-danger",
+}
